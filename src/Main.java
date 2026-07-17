2e8ee2fbc6c8759652c1a9ae10d4fa578d6772ea
KarixD2137 <karol.kopczynski7@gmail.com>
2026-07-17 13:46:53 +0200

    Added calculator

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,32 @@
 void main() {
-    
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter first number: ");
+    int number1 = scanner.nextInt();
+    System.out.print("Select operation [+] [-] [*] [/]: ");
+    char operation = scanner.next().charAt(0);
+    System.out.print("Enter second number: ");
+    int number2 = scanner.nextInt();
+
+    int result;
+    switch (operation)
+    {
+        case '+':
+            result=number1+number2;
+            System.out.println(result);
+            break;
+        case '-':
+            result=number1-number2;
+            System.out.println(result);
+            break;
+        case '*':
+            result=number1*number2;
+            System.out.println(result);
+            break;
+        case '/': // TODO: Fix dividing by zero
+            result=number1/number2;
+            System.out.println(result);
+            break;
+        default:
+            System.out.println("Invalid operation");
+    }
 }
